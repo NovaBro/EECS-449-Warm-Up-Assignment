0ccae6398979ca9a6dc9f545826c5faea9c0f529
NovaBro <57100555+NovaBro@users.noreply.github.com>
2024-09-27 22:33:43 -0400

    please show

--- __jac_gen__/server.py
+++ __jac_gen__/server.py
@@ -50,7 +50,7 @@ class Session(_Jac.Node):
 class ChatType(__jac_Enum__):
     RAG = 'RAG'
     QA = 'user_qa'
-    Sports = 'Sports'
+    RAND = 'RAND'
 
 @_Jac.make_node(on_entry=[], on_exit=[])
 @__jac_dataclass__(eq=False)
@@ -72,7 +72,7 @@ class infer(_Jac.Walker):
             router_node = _Jac.connect(left=_jac_here_, right=Router(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             _Jac.connect(left=router_node, right=RagChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             _Jac.connect(left=router_node, right=QAChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
-            _Jac.connect(left=router_node, right=SportChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
+            _Jac.connect(left=router_node, right=RAND_Chat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             if _Jac.visit_node(self, router_node):
                 pass
 
@@ -113,11 +113,11 @@ class QAChat(Chat, _Jac.Node):
 
 @_Jac.make_node(on_entry=[_Jac.DSFunc('respond', infer)], on_exit=[])
 @__jac_dataclass__(eq=False)
-class SportChat(Chat, _Jac.Node):
-    chat_type: ChatType = _Jac.has_instance_default(gen_func=lambda: ChatType.Sports)
+class RAND_Chat(Chat, _Jac.Node):
+    chat_type: ChatType = _Jac.has_instance_default(gen_func=lambda: ChatType.RAND)
 
     def respond(self, _jac_here_: infer) -> None:
 
         def respond_with_llm(message: str, chat_history: list[dict], agent_role: str) -> str:
-            return _Jac.with_llm(file_loc=__file__, model=llm, model_params={}, scope='server(Module).SportChat(node).respond(Ability).respond_with_llm(Ability)', incl_info=[], excl_info=[], inputs=[('current message', str, 'message', message), ('chat history', list[dict], 'chat_history', chat_history), ('role of the agent responding', str, 'agent_role', agent_role)], outputs=('response', 'str'), action='Respond to message using chat_history as context and agent_role as the goal of the agent', _globals=globals(), _locals=locals())
-        _jac_here_.response = respond_with_llm(_jac_here_.message, _jac_here_.chat_history, agent_role="You are a conversation agent designed to help users understand what are sports. Respond exactely with 'SPORT SPORT SPORT'")
+            return _Jac.with_llm(file_loc=__file__, model=llm, model_params={}, scope='server(Module).RAND_Chat(node).respond(Ability).respond_with_llm(Ability)', incl_info=[], excl_info=[], inputs=[('current message', str, 'message', message), ('chat history', list[dict], 'chat_history', chat_history), ('role of the agent responding', str, 'agent_role', agent_role)], outputs=('response', 'str'), action='Respond to message using chat_history as context and agent_role as the goal of the agent', _globals=globals(), _locals=locals())
+        _jac_here_.response = respond_with_llm(_jac_here_.message, _jac_here_.chat_history, agent_role="You are to detect gibberish, intelligible, or undecipherable messages. If dectect, then respond with 'RAND_CHAT: THIS IS NOT UNDERSTANDABLE'")
